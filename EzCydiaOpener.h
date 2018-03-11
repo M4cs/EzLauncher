4961d6293224efb91c901a61a853b172117e81d0
M4cs <maxlikescs@gmail.com>
2018-03-11 15:48:51 -0500

    INitialFileload

---
+++ EzCydiaOpener.h
@@ -0,0 +1,6 @@
+#import "ControlCenterUIKit/CCUIToggleModule.h"
+#import "UIKit/UIApplication.h"
+
+@interface EzCydiaOpener : CCUIToggleModule
+@property (nonatomic, assign, readwrite) BOOL EzCydiaOpener;
+@end
